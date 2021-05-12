52497f40f288316593c5a262b30d18035eab6f77
David Maignan <davidmaignan@gmail.com>
2021-05-12 13:34:02 -0400

    Refactoring main with devices class wrapper

--- utils/gps.py
+++ utils/gps.py
@@ -1,4 +1,6 @@
 import time
+
+import pynmea2
 import serial
 from multiprocessing.context import Process
 
@@ -8,18 +10,29 @@ class GPS:
         self.port = port
         self.baudrate = baudrate
         self.process = None
+        self.serial = serial.Serial()
+        self.serial.baudrate = self.baudrate
+        self.serial.port = self.port
+        self.file = None
 
     def record(self, file_path: str) -> None:
-        with serial.Serial(self.port, self.baudrate) as ser:
+        with serial.Serial(self.port, self.baudrate) as ser, open(file_path, "w") as _file:
             while True:
                 data = ser.readline().decode('ascii', errors='replace')
-                print(data)
+                if data[1:6] == "GNGGA":
+                    try:
+                        position = pynmea2.parse(data)
+                        value = "{},{},{}\n".format(time.time(), position.latitude, position.longitude)
+                        _file.write(value)
+                        _file.flush()
+                    except:
+                        pass
 
-    def start(self, file_path: str) -> None:
+    def start_recording(self, file_path: str) -> None:
         self.process = Process(target=self.record, args=(file_path,))
         self.process.start()
 
-    def stop(self) -> None:
+    def stop_recording(self) -> None:
         self.process.terminate()
         self.process.join()
 
@@ -27,9 +40,13 @@ class GPS:
 if __name__ == "__main__":
     gps = GPS("/dev/ttyUSB1", 9600)
 
-    print("Start")
-    file = "gps.bin"
-    gps.start(file)
+    print("GPS: start")
+    file = "gps_test.csv"
+    gps.start_recording(file)
     time.sleep(10)
-    gps.stop()
+    gps.stop_recording()
     print("End")
+    time.sleep(3)
+
+
+    print("GPS: stop")
